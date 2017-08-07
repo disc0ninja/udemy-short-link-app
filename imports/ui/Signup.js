9638ab6cc3758acfb165b68a9265ff796e4d6879
disc0ninja <disc0ninja77@gmail.com>
2017-08-07 11:21:10 -0500

    Registrations: Created signup form and added basic state and error handling for signup form

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,11 +2,30 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+  };
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.setState({
+      error: 'Something went wrong'
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Signup for a Shrt Lnk account</h1>
-        {/* signup form here */}
+        {this.state.error ? <p>{this.state.error}</p> : undefined }
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" name="email" placeholder="Email"/>
+          <input type="password" name="password" placeholder="Password" />
+          <button>Create Account</button>
+        </form>
         <Link to="/">Have an account already?</Link>
       </div>
     );
