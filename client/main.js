623d99878f37779a802e9e386b78ce564ef2028b
disc0ninja <disc0ninja77@gmail.com>
2017-08-11 17:46:52 -0500

    Authentication: Added functionality to redirect users based authentication status, and browser location

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { Tracker } from 'meteor/tracker';
 
 // Component Imports
 import Signup from '../imports/ui/Signup';
@@ -9,8 +10,8 @@ import Link from '../imports/ui/Link';
 import NotFound from '../imports/ui/NotFound';
 import Login from '../imports/ui/Login';
 
-window.browserHistory = browserHistory;
-
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = '/links';
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={Login}/>
@@ -19,6 +20,22 @@ const routes = (
     <Route path="*" component={NotFound}/>
   </Router>
 );
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const pathName = browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathName);
+  const isAuthenticatedPage = authenticatedPages.includes(pathName);
+
+  // If on an unauthenticated page and logged in, redirect to /links
+  if (isAuthenticated && isUnauthenticatedPage) {
+    browserHistory.push('/links');
+  // If on an authenticated page and not logged in, redirect to /
+  } else if (!isAuthenticated && isAuthenticatedPage) {
+    browserHistory.push('/');
+  };
+  
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
+import { Accounts } from 'meteor/accounts-base';
 
 export default class Link extends React.Component {
   onLogout() {
-    browserHistory.push('/');
+    Accounts.logout();
   }
     render() {
       return (
         <div>
           <h1>Links</h1>
-          <button onClick={this.onLogout.bind(this)}>Login</button>
+          <button onClick={this.onLogout.bind(this)}>Logout</button>
         </div>
       );
     };
