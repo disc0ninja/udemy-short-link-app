172f44f665f2d3ae21ad16a2f9e7356227f01ef6
disc0ninja <disc0ninja77@gmail.com>
2017-08-11 19:56:49 -0500

    Authentication: Refactor: Moved authentication code into files better suited to keep the application clean and organized

---
+++ imports/routes/routes.js
@@ -0,0 +1,45 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+
+// Component Imports
+import Signup from '../ui/Signup';
+import Link from '../ui/Link';
+import NotFound from '../ui/NotFound';
+import Login from '../ui/Login';
+
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = '/links';
+const onEnterPublicPage = () => {
+  if (Meteor.userId()) {
+    browserHistory.replace('/links');
+  }
+};
+const onEnterPrivatePage = () => {
+  if (!Meteor.userId()) {
+    browserHistory.replace('/');
+  }
+};
+
+export const onAuthChange = (isAuthenticated) => {
+  const pathName = browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathName);
+  const isAuthenticatedPage = authenticatedPages.includes(pathName);
+
+  // If on an unauthenticated page and logged in, redirect to /links
+  if (isAuthenticated && isUnauthenticatedPage) {
+    browserHistory.replace('/links');
+  // If on an authenticated page and not logged in, redirect to /
+  } else if (!isAuthenticated && isAuthenticatedPage) {
+    browserHistory.replace('/');
+  }
+}
+
+export const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
+    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
+    <Route path="/links" component={Link} onEnter={onEnterPrivatePage}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
+);
