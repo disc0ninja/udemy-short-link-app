e740e71c76d1764296c34f4b225a19a328447a67
disc0ninja <disc0ninja77@gmail.com>
2017-08-12 17:05:04 -0500

    Links Feature: Added a link api to handle createing and adding new links to the DB. Added form for link creation to Link component

--- client/main.js
+++ client/main.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 
 import { routes, onAuthChange } from '../imports/routes/routes';
+import { Links } from '../imports/api/links';
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
+});
+
+Tracker.autorun(() => {
+  let allLinks = Links.find().fetch();
+  console.log(allLinks);
 });
 
 Meteor.startup(() => {
---
+++ imports/api/links.js
@@ -0,0 +1,2 @@
+import { Mongo } from 'meteor/mongo';
+export const Links = new Mongo.Collection('links');
