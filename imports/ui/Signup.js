2a2195a83d64bc1eee699d23466fb8a83a277cc4
disc0ninja <disc0ninja77@gmail.com>
2017-08-11 19:25:50 -0500

    Authentication: Create user validations. Handled validation of new user account email on the server (using simpl schema), and added a client side validaition for password length

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -14,6 +14,10 @@ export default class Signup extends React.Component {
     let email = this.refs.email.value.trim()
     let password = this.refs.password.value.trim()
 
+    if (password.length < 9 ) {
+      return this.setState({error: 'Password must be more than 8 characters long'})
+    }
+
     Accounts.createUser({email, password}, (err) => {
       if (err) {
         this.setState({error: err.reason})
@@ -27,7 +31,7 @@ export default class Signup extends React.Component {
       <div>
         <h1>Signup for a Shrt Lnk account</h1>
         {this.state.error ? <p>{this.state.error}</p> : undefined }
-        <form onSubmit={this.onSubmit.bind(this)}>
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
           <input type="email" ref="email" name="email" placeholder="Email"/>
           <input type="password" ref="password" name="password" placeholder="Password" />
           <button>Create Account</button>
