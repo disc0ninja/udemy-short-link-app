9cb24ddc1e0ced7a0d5ca2f6c908231746390cd6
disc0ninja <disc0ninja77@gmail.com>
2017-08-13 11:45:45 -0500

    Links List Item: Copy Feature: Implemented justCopied state that determines the text displayed on the copy button for the link list item. When justCopied is false(default) the users button will display 'Copy', when a user clicks the copy button the state of justCopied will be set to true for 1 second, and while the true, the user will see 'Copied' on the corresponding button

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -2,12 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
 
+// Create a boolean state called justCopied. Default to false
+// On success switch justCopied to true
+// Wait a second. Switch justCopied to false
+
+// Dynamically render the button text. true => Copied. false => Copy
+
 export default class LinksListItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      justCopied: false
+    }
+  }
+
   componentDidMount() {
     this.clipboard = new Clipboard(this.refs.copy);
 
     this.clipboard.on('success', () => {
-      console.log('copied');
+      this.setState({justCopied: true});
+      setTimeout( () => this.setState({ justCopied: false }), 1000);
     }).on('error', () => {
       console.log('unable to copy');
     })
@@ -22,7 +36,7 @@ export default class LinksListItem extends React.Component {
       <div>
         <h3>{this.props.url}</h3>
         <p>{this.props.shortUrl}</p>
-        <button ref="copy" data-clipboard-text={this.props.shortUrl} >Copy</button>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl} >{ this.state.justCopied ? 'Copied' : 'Copy' }</button>
       </div>
     )
   }
