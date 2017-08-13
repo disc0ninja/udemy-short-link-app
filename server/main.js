b0be366ea2f8a65022fb91954fb0dbc96273ccd2
disc0ninja <disc0ninja77@gmail.com>
2017-08-13 09:10:40 -0500

    Links: Added shortid npm module to help with the shortening of links. Implemented a custom middleware to send HTTP 302 redirects, to the shortened url(id) specified

--- server/main.js
+++ server/main.js
@@ -1,9 +1,26 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 
+// API imports
 import '../imports/api/users';
-import '../imports/api/links';
+import { Links } from '../imports/api/links';
+
+// Configuration Imports
 import '../imports/startup/simple-schema-configuration';
 
 Meteor.startup(() => {
+  WebApp.connectHandlers.use( (req, res, next) => {
+    console.log(req.url)
+    const _id = req.url.slice(1);
+    const link = Links.findOne({ _id });
+
+    if (link) {
+      res.statusCode = 302;
+      res.setHeader('Location', link.url);
+      res.end();
+    } else {
+      next();
+    }
+  })
 
 });
