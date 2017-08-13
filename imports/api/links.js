91c8f68e805642fe110ddf01c9dfed94f72b32ac
disc0ninja <disc0ninja77@gmail.com>
2017-08-12 19:22:31 -0500

    Subscriptions and Publications: Removed autopublish from meteor, added userID attribute to newly created links, setup publish and subscirbe functionality for links, so users are able to create links, and are only able to see links they've created

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,2 +1,13 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+
 export const Links = new Mongo.Collection('links');
+
+if (Meteor.isServer) {
+  Meteor.publish('linksPub', function () {
+    return Links.find( { userId: this.userId } );
+  });
+}
+
+Meteor.methods({
+});
