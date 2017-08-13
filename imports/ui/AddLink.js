380cc29027a8fa479c68851b091df49335fef5e6
disc0ninja <disc0ninja77@gmail.com>
2017-08-13 14:46:10 -0500

    Add Link: UX, moved the add link functionality into a modal, via react-modal npm package

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from 'react-modal';
 
 // Custom API Imports
 import { Links } from '../api/links';
@@ -7,7 +8,8 @@ export default class AddLink extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: ''
+      url: '',
+      isOpen: false
     }
   }
 
@@ -26,7 +28,7 @@ export default class AddLink extends React.Component {
     if (url) {
       Meteor.call('links.insert', url, (err, res) => {
         if (!err) {
-          this.setState({ url: ''})
+          this.setState({ url: '', isOpen: false})
         }
       });
     }
@@ -35,13 +37,22 @@ export default class AddLink extends React.Component {
   render() {
     return(
       <div>
-        <p>Add Link</p>
-        <form onSubmit={this.onSubmit.bind(this)}>
-          <input
-             type="text" ref="url" placeholder="URL to shorten" value={this.state.url} onChange={this.onChange.bind(this)}/>
-          <button>Add Link</button>
-        </form>
-    </div>
+        <button onClick={() => this.setState({isOpen: true})}>
+          + Add Link
+        </button>
+        <Modal
+          isOpen={this.state.isOpen}
+          contentLabel="Add link">
+          <p>Add Link</p>
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <input
+               type="text" ref="url" placeholder="URL to shorten" value={this.state.url} onChange={this.onChange.bind(this)}/>
+            <button>Add Link</button>
+          </form>
+          <button onClick={() => this.setState({isOpen: false, url: ''})}>Cancel</button>
+        </Modal>
+
+      </div>
     )
   }
 }
