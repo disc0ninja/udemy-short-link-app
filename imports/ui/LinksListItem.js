b5e06ba72b681149a3576ded6aef5b002a8be176
disc0ninja <disc0ninja77@gmail.com>
2017-08-13 14:34:20 -0500

    Link Analytics: Added moment node package,and used it to help format and display the last visited at information to the user

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
+import moment from 'moment';
 import Links from '../api/links';
 
 // Create a boolean state called justCopied. Default to false
@@ -33,13 +34,24 @@ export default class LinksListItem extends React.Component {
     this.clipboard.destroy();
   }
 
+  renderStats() {
+    const visitMessage = this.props.visitedCount === 1 ? 'visit' : 'visits'
+    let visitedMessage = null;
+
+    if (typeof this.props.lastVisitedAt === 'number') {
+      visitedMessage =`(visited ${moment(this.props.lastVisitedAt).fromNow()})`
+    }
+
+    return <p>{this.props.visitedCount} {visitMessage} - {visitedMessage}</p>
+  }
+
   render() {
     return(
       <div>
         <h3>{this.props.url}</h3>
         <p>{this.props.shortUrl}</p>
         <p>{this.props.visible.toString()}</p>
-        <p>{this.props.visitedCount} - {this.props.lastVisitedAt}</p>
+        {this.renderStats()}
         <button ref="copy" data-clipboard-text={this.props.shortUrl} >{ this.state.justCopied ? 'Copied' : 'Copy' }</button>
         <button onClick={() => { Meteor.call('links.setVisibility', this.props._id, !this.props.visible)}}>
           {this.props.visible ? 'Hide' : 'Unhide'}
