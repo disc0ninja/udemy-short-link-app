728a9f385ac3878d453ae91a19cf7f029c4a1ceb
disc0ninja <disc0ninja77@gmail.com>
2017-08-13 10:54:09 -0500

    Link Copy to Clipboard feature: imported clipboard npm library, initialized clipboard on componentDidMount() and destroyed clipboard listeners on componentWillUnmount(). Created a button in LinksListItem components that copies the shortened url to the client clipboard on click, or logs an error message to the console on error

--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -30,7 +30,7 @@ export default class LinksList extends React.Component {
   renderLinksListItems() {
     return this.state.links.map( (link) => {
       const shortUrl = Meteor.absoluteUrl(link._id);
-      return <LinksListItem key={link._id} shortUrl={/*shortUrl*/} {...link}/>
+      return <LinksListItem key={link._id} shortUrl={shortUrl} {...link}/>
     })
   }
 
--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refs.copy);
+
+    this.clipboard.on('success', () => {
+      console.log('copied');
+    }).on('error', () => {
+      console.log('unable to copy');
+    })
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+
   render() {
     return(
       <div>
         <h3>{this.props.url}</h3>
         <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl} >Copy</button>
       </div>
     )
   }
