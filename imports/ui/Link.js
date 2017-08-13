defbc201b09ad0377299af9ea498a6358239d2cf
disc0ninja <disc0ninja77@gmail.com>
2017-08-13 10:06:25 -0500

    Refactor: Converted Link, NotFound, and PrivateHeader Container Components into Stateless Functional Components

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -6,14 +6,12 @@ import PrivateHeader from './PrivateHeader';
 import AddLink from './AddLink';
 import LinksList from './LinksList';
 
-export default class Link extends React.Component {
-  render() {
-    return (
-      <div>
-        <PrivateHeader title="Your Links"/>
-        <LinksList />
-        <AddLink />
-      </div>
-    );
-  };
-};
+export default () => {
+  return(
+    <div>
+      <PrivateHeader title="Your Links"/>
+      <LinksList/>
+      <AddLink/>
+    </div>
+  )
+}
--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export default class NotFound extends React.Component {
-  render() {
-    return <p>NotFound Component</p>
-  };
+export default () => {
+  return(
+    <div>
+      <p>NotFound Component Here</p>
+    </div>
+  );
 };
